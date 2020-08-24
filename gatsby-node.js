ef56dcd2317b6837d65558e748c563a4a95c4569
Erico Almeida <ericoalmeidamvp@gmail.com>
2020-08-24 07:22:52 -0400

    Loaded data from blog post and shown on the screen

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const { resolve } = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
 //To add the slug field to each post
@@ -20,4 +21,39 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: `${slug.slice(12)}`,
     })
   }
+}
+
+//To create pages
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  // query para buscar todos os posts
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(({ data }) => {
+    //Percorre a lista de posts criando uma pagina para cada um deles
+    const {
+      allMarkdownRemark: { edges },
+    } = data
+
+    edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: resolve(__dirname, 'src', 'templates', 'blog-post.js'),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
 }
